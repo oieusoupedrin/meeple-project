17bccd3a950b845e3fa7014a1506e1a079f2355f
oieusoupedrin <phr.pereira1992@gmail.com>
2022-08-31 20:14:20 -0300

    First commit with a new React

---
+++ src/pages/home/home.js
@@ -0,0 +1,15 @@
+import './home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Home component
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
